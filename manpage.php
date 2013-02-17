a4e496adbcccbb314094f14f42da9f12f220f111
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-02-17 16:59:27 +0100

    Renamed the "Introduction" nav entry to "Gnuplot basics"

--- manpage.php
+++ manpage.php
@@ -9,25 +9,25 @@ get_header('manpage'); ?>
 
   <!-- ======= Navbar ======= -->
   <ul class="nav nav-pills">
-    <li><a href="http://127.0.0.1/wordpress/">Home</a></li>
+    <li><a href="<?php echo site_url(); ?>">Home</a></li>
     <li class="dropdown">
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Introduction<b class="caret"></b></a>
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Gnuplot basics<b class="caret"></b></a>
       <ul class="dropdown-menu">
-        <li><a href="http://127.0.0.1/wordpress/?page_id=51">Plotting functions</a></li>
-        <li><a href="http://127.0.0.1/wordpress/?page_id=101">Plotting data</a></li>
-        <li><a href="http://127.0.0.1/wordpress/?page_id=114">Output terminals</a></li>
+        <li><a href="<?php echo site_url(); ?>/?page_id=51">Plotting functions</a></li>
+        <li><a href="<?php echo site_url(); ?>/?page_id=101">Plotting data</a></li>
+        <li><a href="<?php echo site_url(); ?>/?page_id=114">Output terminals</a></li>
       </ul>
     </li>
-    <li class="active dropdown">
+    <li class="dropdown active">
       <a href="#" class="dropdown-toggle" data-toggle="dropdown">Manpages<b class="caret"></b></a>
       <ul class="dropdown-menu">
-        <li><a href="http://127.0.0.1/wordpress/?page_id=1284">Gnuplot 4.6</a></li>
+        <li><a href="<?php echo site_url(); ?>/?page_id=1284">Gnuplot 4.6</a></li>
       </ul>
     </li>
-    <!--<li><a href="http://www.gnuplotting.org/gallery.html">Gallery</a></li>-->
-    <li><a href="http://127.0.0.1/wordpress/?page_id=97">About</a></li>
-    <?php if (is_user_logged_in()) { ?><li><a href="http://www.gnuplotting.org/wp-admin">Dashboard <i class="icon-arrow-right"></i></a></li><?php } ?>
-    <li class="feed right"><a href="http://www.gnuplotting.org/feed/"><i class="icon-rss icon-large"></i></a></li>
+    <!--<li><a href="<?php echo site_url(); ?>/gallery.html">Gallery</a></li>-->
+    <li><a href="<?php echo site_url(); ?>/?page_id=97">About</a></li>
+    <?php if (is_user_logged_in()) { ?><li><a href="<?php echo site_url(); ?>/wp-admin">Dashboard <i class="icon-arrow-right"></i></a></li><?php } ?>
+    <li class="feed right"><a href="<?php echo site_url(); ?>/feed/"><i class="icon-rss icon-large"></i></a></li>
   </ul>
 
   <!-- Grid for main content and sidebar -->
