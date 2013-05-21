45c74eb9d1d87e33e87e99af8831ec21e78b9af1
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-05-21 23:49:36 +0200

    start working to include a search in the nav bar

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div class="input-append">
-        <input class="span2" type="text" value="" name="s" id="s" />
+    <div class="input-append span3">
+        <input class="" type="text" value="" name="s" id="s" />
         <button class="btn" type="button" id="searchsubmit" value="Search"><i class="icon-search"></i></button>
     </div>
 </form>
