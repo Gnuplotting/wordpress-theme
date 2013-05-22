9a832e293532fc8c7df970bab3ecda27515840c7
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-05-22 15:16:04 +0200

    Further work on search. Now only the button is missing

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div class="input-append span3">
-        <input class="" type="text" value="" name="s" id="s" />
-        <button class="btn" type="button" id="searchsubmit" value="Search"><i class="icon-search"></i></button>
+    <div class="span3">
+        <input class="" type="text" placeholder="Search" value="" name="s" id="s" />
+        <!--<button class="btn" type="button" id="searchsubmit" value="Search"><i class="icon-search"></i></button>-->
     </div>
 </form>
