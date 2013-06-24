6a4f2d75ca3f5ee081e0ae4c3cfcba60c875e485
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-06-24 13:48:33 +0200

    Updated search box to javascript for changing of color and focussing input

--- searchform.php
+++ searchform.php
@@ -1,6 +1,14 @@
 <div class="span3">
     <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-        <input class="" onclick="this.value='';" type="text" placeholder="Search here ..." value="<?php echo wp_specialchars($s); ?>" name="s" id="s" />
+        <input value="<?php 
+            /* check if we have a search string. If not put 'Search here ...' in the field */
+            $value=wp_specialchars($s); if(empty($value)) { echo 'Search here ...'; } else { echo wp_specialchars($s); }?>" 
+            onfocus="this.style.color='#393318';"
+            onclick="if(this.value=='Search here ...') this.value=''; else this.select();" 
+            onblur="if(this.value=='') { this.value='Search here ...'; this.style.color='#999'; }"
+            type="text" name="s" id="s" />
+        <!-- Set color to gray for the dummy 'Search here ...' text -->
+        <script>var s=document.getElementById('s'); if(s.value=='Search here ...') s.style.color='#999';</script>
         <button class="search-btn" type="submit" id="searchsubmit"></button>
     </form>
 </div>
