4ccc6203738878eb6f74aef6ba4e52a1a2bc4b19
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-06-24 12:49:51 +0200

    Fixed position and size of search box and changing to png icons

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <div class="span3">
     <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
         <input class="" onclick="this.value='';" type="text" placeholder="Search here ..." value="<?php echo wp_specialchars($s); ?>" name="s" id="s" />
-        <button class="search-btn" type="submit" id="searchsubmit"><i class="icon-search icon-large"></i></button>
+        <button class="search-btn" type="submit" id="searchsubmit"></button>
     </form>
 </div>
