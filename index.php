7ae653506403e1413b5342f5c4a8219a80f38981
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2014-01-11 00:00:53 +0100

    Added twitter icon

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ get_header(); ?>
     <!--<li><a href="<?php echo site_url(); ?>/gallery.html">Gallery</a></li>-->
     <li><a href="<?php echo site_url(); ?>/about/">About</a></li>
     <?php if (is_user_logged_in()) { ?><li><a href="<?php echo site_url(); ?>/wp-admin">Dashboard <i class="icon-arrow-right"></i></a></li><?php } ?>
-    <!--li class="feed right"><a href="<?php echo site_url(); ?>/feed/"><i class="icon-twitter icon-large"></i></a></li>-->
+    <li class="right"><a href="http://twitter.com/gnuplotting"><i class="icon-twitter icon-large"></i></a></li>
     <li class="search right"><?php include ("searchform.php"); ?></li>
   </ul>
 
